d97aebc64596fad0ba66d06b3d9a41a060faa663
Mansii-bit <mansi.knp19@gmail.com>
2025-08-31 12:27:25 +0530

    Time: 146 ms (1.24%) | Memory: 378.2 MB (4.39%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -3,12 +3,26 @@ public:
 void solve(vector<int> nums , int low , int high , int &ans){
     if(low>high)return;
    int mid=(low+high)/2;
-    if(nums[low]<=nums[mid])ans=min(ans,nums[low]);
-    solve(nums,low,mid-1,ans);
-    if(nums[mid]<=nums[high])ans=min(ans,nums[mid]);
-    solve(nums,mid+1,high,ans);
+    if(nums[low]<nums[mid]){
+        ans=min(ans,nums[low]);
+        solve(nums,mid+1,high,ans);
+    }
+    else{
+        ans=min(ans,nums[mid]);
+      solve(nums,low,mid-1,ans);  
+    } 
+    if(nums[mid]<nums[high]){
+        ans=min(ans,nums[mid]);
+        solve(nums,low,mid-1,ans);
+    }
+    else {
+        ans=min(ans,nums[high]);
+        solve(nums,mid+1,high,ans);
+    }
 }
     int findMin(vector<int>& nums) {
+        if(nums.size()==1)return nums[0];
+        else if(nums.size()==2)return min(nums[0],nums[1]);
         int ans=INT_MAX;
         int low=0;
         int high=nums.size()-1;
