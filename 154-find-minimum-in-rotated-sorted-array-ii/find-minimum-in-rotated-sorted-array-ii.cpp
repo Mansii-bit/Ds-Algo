c8b35ace995626c5e70329ba50b10479dcc7fdbb
Mansii-bit <mansi.knp19@gmail.com>
2025-08-31 12:48:36 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (64.32%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-void solve(vector<int> nums , int low , int high , int &ans){
-    if(low>high)return;
-   int mid=(low+high)/2;
-    if(nums[low]<nums[mid]  && nums[mid]>=nums[high]){
-        ans=min(ans,nums[low]);
-        solve(nums,mid+1,high,ans);
-    }else if(nums[mid]<nums[high] && nums[low]>=nums[mid]){
-        ans=min(ans,nums[mid]);
-        solve(nums,low,mid-1,ans);
-    }
-    else{
-        ans=min(ans,nums[mid]);
-        solve(nums,mid+1,high,ans);
-        solve(nums,low,mid-1,ans);
-    }
-}
     int findMin(vector<int>& nums) {
-        if(nums.size()==1)return nums[0];
-        else if(nums.size()==2)return min(nums[0],nums[1]);
-        int ans=INT_MAX;
-        int low=0;
-        int high=nums.size()-1;
-        solve(nums,low,high,ans);
-        return ans;
+    int low = 0, high = nums.size() - 1;
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] > nums[high]) {
+            low = mid + 1; // pivot in right half
+        } else if (nums[mid] < nums[high]) {
+            high = mid;    // pivot in left half (including mid)
+        } else {
+            high--;        // nums[mid] == nums[high], shrink search
+        }
     }
+    return nums[low];
+}
+
 };
