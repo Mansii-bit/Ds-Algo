12c4e49e2237567d33cda333857cc615b6906cf9
Mansii-bit <mansi.knp19@gmail.com>
2025-09-04 14:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (48.47%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        return (long long)m*n/2;
+        long long n1=n/2;
+        long long n2=(n+1)/2;
+        long long m1=m/2;
+        long long m2=(m+1)/2;
+        return ((n1*m2) + (n2*m1));
     }
 };
