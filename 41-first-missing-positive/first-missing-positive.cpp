358cf15abb134209dbdbb2217b8bf8c5d5ba7509
Mansii-bit <mansi.knp19@gmail.com>
2025-09-05 11:37:56 +0530

    Time: 29 ms (20.66%) | Memory: 55 MB (48.29%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
+bool solve(vector<int>&nums,int target){
+    int low=0;
+    int high=nums.size()-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(target==nums[mid])return true;
+        else if (target <nums[mid])high=mid-1;
+        else low=mid+1;
+    }
+    return false;
+}
     int firstMissingPositive(vector<int>& nums) {
-        set<int>s;
-        for(int i : nums) s.insert(i);
+        sort(nums.begin(),nums.end());
         int i=1;
-        while(s.count(i)==1){
+        while(solve(nums,i)){
             i++;
         }
         return i;
