350bc09d2f7369f51ac4312a6cb746fb6eb06d77
Mansii-bit <mansi.knp19@gmail.com>
2025-10-08 22:36:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (16.76%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -6,31 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        if(!root) return ans;
-        vector<vector<int>> v;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int size=q.size();
-            vector<int>temp;
-            while(size--){
-            TreeNode* node=q.front();
-            temp.push_back(node->val);
-            if(node->left)q.push(node->left);
-            if(node->right)q.push(node->right);
-            q.pop();
+        vector<int>ans;
+        if(!root)return ans;
+        map<int, TreeNode*> mapy;
+        queue<pair<int, TreeNode*>> q;
+        q.push({0, root});
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                auto x = q.front();
+                int vtl = x.first;
+                TreeNode* node = x.second;
+                q.pop();
+                mapy[vtl] = node;
+                if (node->left)
+                    q.push({vtl +1, node->left});
+                if (node->right)
+                    q.push({vtl + 1, node->right});
             }
-            v.push_back(temp);
         }
-        for( auto i : v){
-            ans.push_back(i.back());
+        for(auto x: mapy){
+            ans.push_back(x.second->val);
         }
         return ans;
     }
