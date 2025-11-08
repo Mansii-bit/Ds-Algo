6634cd62a7fefb5e4ceed7802f3c5898aa58b396
Mansii-bit <mansi.knp19@gmail.com>
2025-11-08 15:45:35 +0530

    Time: 4 ms (62.24%) | Memory: 16.6 MB (6.42%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -46,7 +46,7 @@ vector<int> pse(int n , vector<int>arr){
     vector<int>a(n);
     stack<int>st;
     for(int i =0;i<n;i++){
-        while(!st.empty() && arr[st.top()]>=arr[i]){
+        while(!st.empty() && arr[st.top()]>arr[i]){
             st.pop();
         }
         a[i]=st.empty() ? -1 : st.top();
@@ -58,7 +58,7 @@ vector<int> nse(int n , vector<int>arr){
     vector<int>a(n);
     stack<int>st;
     for(int i =n-1;i>=0;i--){
-        while(!st.empty() && arr[st.top()]>arr[i]){
+        while(!st.empty() && arr[st.top()]>=arr[i]){
             st.pop();
         }
         a[i]=st.empty() ? n : st.top();
