84147bbc78da31d7a91d6fa317fe7b1802349d0d
Mansii-bit <mansi.knp19@gmail.com>
2025-11-21 21:01:29 +0530

    Time: 15 ms (16.60%) | Memory: 11.4 MB (5.66%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -7,17 +7,13 @@ public:
         int l=0,r=0;
         int ans=0;
         while(r<n){
-            // if(mapy.find(s[r])!=mapy.end()){
-            //     if(mini.first==s[r]) mini.second++;
-            // }else mini={s[r],1};
               mapy[s[r]]++;
               maxi=max(mapy[s[r]],maxi);
 
-              while((r-l+1) - maxi >k  ){
+              if((r-l+1) - maxi >k  ){
                 if(mapy[s[l]]==1) mapy.erase(s[l]);
                 else mapy[s[l]]--;
                 l++;
-                // mini.second=min(mapy[s[l]] , mini.second);
                 for(auto x : mapy){
                     maxi=max(maxi,x.second);
                 }
