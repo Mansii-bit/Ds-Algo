7f54b859cae8360a896c928dcdb87c7f25a5b4f5
Mansii-bit <mansi.knp19@gmail.com>
2025-11-25 10:04:50 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (59.69%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
+int sum(vector<int>& nums, int goal){
+    if(goal<0) return 0;
+    int n=nums.size();
+    int l=0,r=0;
+    int sum=0,cnt=0;
+    while(r<n){
+        sum+=nums[r];
+        while(sum>goal){
+            sum-=nums[l];
+            l++;
+        }
+        cnt+=(r-l+1);
+        r++;
+    }
+    return cnt;
+}
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         int n=nums.size();
-        int psum=0;
-        int cnt=0;
-        unordered_map<int,int>mapy;
-        mapy[0]=1;
-        for(int i : nums){
-            psum+=i;
-            if(mapy.find(psum-goal) != mapy.end())cnt+=mapy[psum-goal];
-            mapy[psum]++;
-        }
-        return cnt;
+        int cnt1=sum(nums,goal);
+        int cnt2=sum(nums,goal-1);
+        return cnt1-cnt2;
     }
 };
