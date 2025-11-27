5a1932d7e5ef0970e331aed5261f65a4c3a8c981
Mansii-bit <mansi.knp19@gmail.com>
2025-11-27 12:20:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (69.34%) - LeetSync

---
+++ 290-word-pattern/word-pattern.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        map<string, char>mapy;
+        set<int>sett;
+        string word="";
+        string ans="";
+        int j=-1;
+        for(int i=0;i<(int)s.size();i++){
+            if(s[i]==' '){
+                j++;
+                if(mapy.find(word)!= mapy.end()){
+                    if(pattern[j]!=mapy[word])return false;
+                }else{
+                    if(sett.count(pattern[j])==0)mapy[word]=pattern[j];
+                    else return false;
+                }
+                sett.insert(pattern[j]);
+               word="";
+               continue;
+            }
+            word+=s[i];
+        }
+            j++;
+        if(mapy.find(word)!= mapy.end()){
+                    if(pattern[j]!=mapy[word])return false;
+        }else {
+ if(sett.count(pattern[j])==0)mapy[word]=pattern[j];
+                    else return false;
+        }
+        
+        // for(char c: pattern) mapy2[c]++;
+        // if(mapy.size()!=mapy2.size())return false;
+        // int cnt=0;
+        // for(auto  x : mapy){
+        //     cnt+=x.second;
+        // }
+        // return n==cnt;
+
+        return j==pattern.size()-1 ? true : mapy.size()==sett.size();
+    }
+};
