39814bde5654ce6d4304dcfe3803db70bde440bd
Mansii-bit <mansi.knp19@gmail.com>
2025-11-27 14:18:27 +0530

    Time: 48 ms (70.70%) | Memory: 14.4 MB (46.49%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-int lci(int ind , int prev , vector<int>&nums , int n , vector<vector<int>>&memo){
-    if(ind>=n)return 0;
-    if(memo[ind][prev+1]!=-1) return memo[ind][prev+1];
-    int notTake=0+lci(ind+1,prev,nums,n,memo);
-    int take= (prev==-1 || nums[prev]<nums[ind]) ? 1+lci(ind+1,ind,nums,n,memo) : 0;
-    return memo[ind][prev+1]= max(take,notTake);
-}
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>memo(n,vector<int>(n+1,-1));
-        return lci(0,-1,nums,n,memo);
+        vector<int>dp(n,1);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        int ans=-1;
+        for(int i : dp) ans=max(i,ans);
+        return ans;
     }
 };
