35250ca17a1bd4a273950f95c078148297e9e2ca
Mansii-bit <mansi.knp19@gmail.com>
2025-12-23 23:48:12 +0530

    Time: 40 ms (67.34%) | Memory: 45.5 MB (40.36%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& arr, int k) {
+    int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int>mapy;
-        int n=arr.size();
-        int cnt=0;
-        int sum=0;
+        int n=nums.size();
+        int sum=0,ans=0;
         mapy[0]=1;
-        for(int i=0;i<n;i++){
-            sum+=arr[i];
-            int rem=sum-k;
-            if(mapy.find(rem)!=mapy.end()){
-                cnt+=mapy[rem];
-            }
+        for(int i =0;i<n;i++){
+            sum+=nums[i];
+            if(mapy.find(sum-k)!=mapy.end())ans+=mapy[sum-k];
             mapy[sum]++;
         }
-        return cnt;
+        return ans;
     }
 };
