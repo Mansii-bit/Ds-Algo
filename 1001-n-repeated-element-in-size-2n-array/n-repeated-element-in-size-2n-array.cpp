2db2ff5b33b2953b10b144d6fd6835c201668380
Mansii-bit <mansi.knp19@gmail.com>
2026-01-02 15:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 28.6 MB (53.60%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -3,10 +3,11 @@ public:
     int repeatedNTimes(vector<int>& nums) {
         int n=nums.size();
         unordered_map<int,int>mapy;
-        for(int i : nums)mapy[i]++;
-        for(auto x : mapy){
-            if(x.second==n/2)return x.first;
+        for(int i : nums){
+            if(mapy.find(i)!=mapy.end())return i;
+            mapy[i]++;
         }
+      
         return 1;
     }
 };
